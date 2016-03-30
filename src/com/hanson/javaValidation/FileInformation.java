b5c30a969aebfcc6b5ab3ad25fc42598dd6e83c4
Mitchell Hanson <mitchhanson43@gmail.com>
2016-03-30 10:09:58 -0500

    refactored the quote and comment text removal code into its own class called QuoteAndCommentReplacer.  The getTransformedText() method returns an array list with comments and quotes replace with dots.  Must still fix single line comment issues and ensure the correct text is being removed (off by one errors)

--- src/com/hanson/javaValidation/FileInformation.java
+++ src/com/hanson/javaValidation/FileInformation.java
@@ -16,14 +16,15 @@ public class FileInformation {
     private List<String> keywords;
     //private final keywordDecode keywordDecoder = new keywordDecode();
     private FileParser fileParser;
+    private QuoteAndCommentReplacer quoteAndTextReplacer;
     private List<int[]> lineInformation;
-    private List<Integer> javadocComments;
+    //private List<Integer> javadocComments;
 
     private List<String> fileContents;
 
     //private boolean previousLineJavadocComment = false;
-    private boolean javadocCommentOpen = false;
-    private boolean multiLineCommentOpen = false;
+    //private boolean javadocCommentOpen = false;
+    //private boolean multiLineCommentOpen = false;
 
     //******************************************************************************************************/
     // These variables are for finding class bounds.  Should be refactored into its own class most likely.
@@ -46,18 +47,18 @@ public class FileInformation {
     //******************************************************************************************************/
     // These variables are for stripping comment and quoted text.  Should be refactored into its own class most likely.
     //******************************************************************************************************
-    private Boolean openQuote = false;
+    private boolean openQuote = false;
     private int quoteOpenIndex;
     private int multiLineOpenCommentIndex;
     private int multiLineOpenLine;
-    private List<String> quotedTextArray = new ArrayList<>();
-    private Boolean openMultiLineComment = false;
-    private Boolean openJavadocComment = false;
-    private Boolean afterSingleLineComment = false;
-    private Boolean previousCharacterIsEscape = false;
-    private List<int[]> charactersToRemove = new ArrayList<>();  // Three values, line number, opening quote, closing quote
-    private List<Integer> linesToReplace = new ArrayList<>();    // Replace the entire line for comments?
 
+    private boolean openMultiLineComment = false;
+    private boolean openJavadocComment = false;
+    private boolean afterSingleLineComment = false;  // Needed for removing comments after // - this broke somehow
+    private boolean previousCharacterIsEscape = false;
+
+    private List<int[]> charactersToRemove = new ArrayList<>();  // Four values, opening line number, opening quote, ending line number, closing quote/
+    //private List<String> quotedTextArray = new ArrayList<>();  // Used for printing
 
     //******************************************************************************************************
     //******************************************************************************************************
@@ -65,9 +66,10 @@ public class FileInformation {
 
     FileInformation(FileParser parser) {
         lineInformation = new ArrayList<>();
-        javadocComments = new ArrayList<>();
+        //javadocComments = new ArrayList<>();
         keywords = new ArrayList<>();
         fileParser = parser;
+        quoteAndTextReplacer = new QuoteAndCommentReplacer(parser.getFileContents());
     }
 
     public List getLineINformation() {
@@ -80,10 +82,11 @@ public class FileInformation {
      */
     public void runFileInformation() {
         //previousLineJavadocComment = false;
-        javadocCommentOpen = false;
-        multiLineCommentOpen = false;
+        //javadocCommentOpen = false;
+        //multiLineCommentOpen = false;
 
-        fileContents = fileParser.getFileContents();
+        fileContents = quoteAndTextReplacer.getTransformedText();
+
         generateKeywordList();
 
         System.out.println(fileContents.size());
@@ -92,23 +95,31 @@ public class FileInformation {
         }
         System.out.println("Class Opening:  " + openingClassLine + ". Class closing: " + closingClassLine);
         findFunctionBounds();
-        replaceQuotedText();
+        //replaceQuotedText();
         printFunctionBounds();
         printTextToRemove();
     }
 
     private void runInfoGatherers(int lineNumber, String lineText) {
-        checkJavadocComment(lineNumber, lineText);
+        // checkJavadocComment(lineNumber, lineText);
         findClassBounds(lineNumber, lineText);
-        removeQuotedAndCommentedText(lineNumber, lineText);
+        // removeQuotedAndCommentedText(lineNumber, lineText);
         for (String keyword : keywords) {
             checkKeyword(lineNumber, lineText, keyword);
         }
     }
 
+    /*
+        TODO:  Remove this function.
+        This function doesn't needs to exist.  Another function is removing the text within javadocs.  If recording the
+        bounds is needed, that function can handle that as well.
+     */
+    /*
     private void checkJavadocComment(int lineNumber, String lineText) {
         if (javadocCommentOpen) {
-            if (lineText.contains("*/")) {
+        */
+            //if (lineText.contains("*/")) {
+    /*
                 javadocCommentOpen = false;
             }
             addLineInformation(lineNumber, JAVADOC_CODE);
@@ -122,10 +133,13 @@ public class FileInformation {
             checkMultiLineComment(lineNumber, lineText);
         }
     }
-
+    */
+    /*
     private void checkMultiLineComment(int lineNumber, String lineText) {
         if (multiLineCommentOpen) {
-            if (lineText.contains("*/")) {
+    */
+            //if (lineText.contains("*/")) {
+    /*
                 multiLineCommentOpen = false;
             }
             addLineInformation(lineNumber, MULTI_LINE_COMMENT_CODE);
@@ -135,7 +149,7 @@ public class FileInformation {
             addLineInformation(lineNumber, MULTI_LINE_COMMENT_CODE);
         }
     }
-
+    */
     private void checkKeyword(int lineNumber, String lineText, String keyword) {
         if (lineText.contains(keyword)) {
             System.out.println("found code: " + keywordDecode.decodeKeyword(keyword) + ". On line " + lineNumber + ".  Keyword: " + keyword);
@@ -216,204 +230,6 @@ public class FileInformation {
                 }
             }
         }
-    }
-
-    private void removeQuotedAndCommentedText(int lineNumber, String lineText) {
-        /*
-        private Boolean openQuote = false;
-        private int quoteOpenIndex;
-        private int multiLineOpenComment;
-        private int multiLineOpenLine;
-        private Boolean openMultiLineComment = false;
-        private Boolean openJavadocComment = false;
-        private Boolean afterSingleLineComment = false;
-        private Boolean previousCharacterIsEscape = false;
-        private List<int[]> charactersToRemove = new ArrayList<>();  // Three values, line number, opening quote, closing quote
-        private List<Integer> linesToReplace = new ArrayList<>();    // Replace the entire line for comments?
-        */
-
-        /*
-         *  This loop looks at all characters in the passed string.  It will create int arrays and add them to the
-         *  charactersToRemove for all text between quotes, multi-line comments, and javadoc comments, as well as any
-         *  text after single line comments.  It tracks whether it is processing a text within a quote, comment, ..., with
-         *  boolean variables and ignore certain characters if it's within various areas of text (for example:  quotations
-         *  are not logged to be removed if found inside of a multi-line comment).
-         */
-        for (int index = 0; index < lineText.length(); index ++) {
-
-            /*
-              * Search for a quotation mark not preceded by an escape character.  If found add bounds of quoted text to
-              * an int array and add that array to the charactersToRemoveList and set openQuote to false;
-             */
-            if (openQuote) {
-                if (lineText.charAt(index) == '\"') {
-                    if (previousCharacterIsEscape) {
-                        previousCharacterIsEscape = false;
-                    } else {
-                        int[] tempArray = {lineNumber, quoteOpenIndex, lineNumber, index};
-                        charactersToRemove.add(tempArray);
-                        quotedTextArray.add(lineText.substring(quoteOpenIndex + 1, index));
-                        openQuote = false;
-                    }
-
-                } else if (lineText.charAt(index) == '\\') {
-                    previousCharacterIsEscape = true;
-                }
-            /*
-             * Search for a closing javadoc symbol.
-             */
-            }  else if (openJavadocComment) {
-                // Check for first character of closing comment
-                if (lineText.charAt(index) == '*' && (index < lineText.length() - 1)) {
-                    // If first character is found, check it isn't the last character in the string, and that the next
-                    // character is the second character of the closing comment
-                    if (lineText.charAt(index + 1) == '/') {
-                        System.out.println("Found closing javadoc");
-                        int[] tempArray = {multiLineOpenLine, multiLineOpenCommentIndex, lineNumber, index};
-                        charactersToRemove.add(tempArray);
-                        openJavadocComment = false;
-                    }
-                }
-            }
-            else if (openMultiLineComment) {
-                // Check for first character of closing comment
-                if (lineText.charAt(index) == '*' && (index < lineText.length() - 1)) {
-                    // If first character is found, check it isn't the last character in the string, and that the next
-                    // character is the second character of the closing comment
-                    if (lineText.charAt(index + 1) == '/') {
-                        System.out.println("Found closing multi-line comment");
-                        int[] tempArray = {multiLineOpenLine, multiLineOpenCommentIndex, lineNumber, index};
-                        charactersToRemove.add(tempArray);
-                        openMultiLineComment = false;
-                    }
-                }
-            }
-            else {
-                // Set index and booleans if quote found
-                if (lineText.charAt(index) == '\"') {
-                    openQuote = true;
-                    quoteOpenIndex = index;
-                }
-
-                // Look for multi-line comments and javadocs, if there are at least two characters following the /
-                else if (lineText.charAt(index) == '/' ) {
-                    if (index < lineText.length() - 2) {
-
-                        // If followed by two * then it's a javadoc
-                        if (lineText.charAt(index + 1) == '*' && lineText.charAt(index + 2) == '*') {
-                            System.out.println("FOUND JAVADOC COMMENT ONE LINE: " + lineNumber);
-                            openJavadocComment = true;
-                            multiLineOpenCommentIndex = index + 2;
-                            multiLineOpenLine = lineNumber;
-                            index += 2;
-                        }
-
-                        // If followed by one * then it's a multi-line comment
-                        else if (lineText.charAt(index + 1) == '*') {
-                            System.out.println("FOUND MULTI-LINE COMMENT ONE LINE: " + index);
-                            openMultiLineComment = true;
-                            multiLineOpenCommentIndex = index + 1;
-                            multiLineOpenLine = lineNumber;
-                            index += 1;
-                        } else if (lineText.charAt(index + 1) == '/') {
-                            System.out.println("FOUND SINGLE ON LINE: " + lineNumber);
-                            System.out.println("Line Number: " + lineNumber + " start Index: " + (index + 2) + " Line Number: " + lineNumber + ". Line Length: " + (lineText.length() - 1));
-                            int[] tempArray = {lineNumber, index + 2, lineNumber, lineText.length() - 1};
-                            charactersToRemove.add(tempArray);
-                            return;
-                        }
-                    }
-
-                    // Check only for multi-line comment if there is only one character following the /
-                    else if (index < lineText.length() - 1) {
-                        if (lineText.charAt(index + 1) == '*') {
-                            System.out.println("FOUND MULTI-LINE COMMENT ON LINE: " + lineNumber);
-                            openMultiLineComment = true;
-                            multiLineOpenCommentIndex = index + 1;
-                            multiLineOpenLine = lineNumber;
-                            index += 1;
-                        } else if (lineText.charAt(index + 1) == '/') {
-                            System.out.println("FOUND SINGLE LINE COMMENT ON LINE: " + lineNumber);
-                            int[] tempArray = {lineNumber, index, lineNumber, lineText.length() - 1};
-                            charactersToRemove.add(tempArray);
-                            return;
-                        }
-
-
-                    }
-                }
-            }
-        }
-    }
-    private void replaceQuotedText() {
-        for(int index = 0; index < charactersToRemove.size(); index ++) {
-
-            int[] tmpArray = charactersToRemove.get(index);
-
-            // If the text to replace spans only one line, use this simple code.  Else, remove text from each line.
-            if (tmpArray[0] == tmpArray[2]) {
-                if (tmpArray[1] >= tmpArray[3]) {
-                    String replacement = buildDots(tmpArray[1], tmpArray[3]);
-
-                    String lineText = fileContents.get(tmpArray[0] - 1);
-                    String newText = lineText.substring(0, tmpArray[1]) + replacement + lineText.substring(tmpArray[3] + 1);
-
-                    fileContents.set(tmpArray[0] - 1, newText);
-
-                    System.out.println(newText);
-                }
-
-            } else {
-
-                // Replace all text between the first line number first index and second line number second index
-                for (int currentLineNumber = tmpArray[0]; currentLineNumber <= tmpArray[2]; currentLineNumber ++) {
-
-                    // If this is the last line of text to replace, replace from index 0 to the closing index of the comment
-                    if (currentLineNumber == tmpArray[2]) {
-                        String replacement = buildDots(0, tmpArray[3]);
-                        String lineText = fileContents.get(currentLineNumber - 1);
-
-                        String newText = replacement + lineText.substring(tmpArray[3] );
-
-                        fileContents.set(tmpArray[2] - 1, newText);
-
-                        System.out.println("Line Number: " + currentLineNumber + ". New text: " + newText);
-                    }
-
-                    // If this is the first line of text to replace, replace from the opening index to the end of the string
-                    else if (currentLineNumber == tmpArray[0]) {
-                        String lineText = fileContents.get(currentLineNumber - 1);
-                        String replacement = buildDots(tmpArray[1], lineText.length() - 1);
-
-                        String newText = lineText.substring(0, tmpArray[1]) + replacement;
-
-                        fileContents.set(tmpArray[0] -1, newText);
-                        System.out.println("Line Number: " + currentLineNumber + ". New text: " + newText);
-                    }
-
-                    // This is a middle line that should entirely be replaced because it is entirely encompassed by
-                    // a multi-line comment
-                    else {
-                        String lineText = fileContents.get(currentLineNumber - 1 );
-                        String replacement = buildDots(0, lineText.length() - 1);
-
-                        fileContents.set(currentLineNumber - 1, replacement);
-                        System.out.println("Line Number: " + currentLineNumber + ". New text: " + replacement);
-                    }
-                }
-            }
-
-
-        }
-    }
-
-    private String buildDots(int startIndex, int endIndex) {
-        String dots = "";
-        for (int i = 0; i < (endIndex - startIndex); i ++) {
-            dots += ".";
-        }
-
-        return dots;
     }
 
     private void printTextToRemove() {
---
+++ src/com/hanson/javaValidation/QuoteAndCommentReplacer.java
@@ -0,0 +1,222 @@
+package com.hanson.javaValidation;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by student on 3/30/16.
+ */
+public class QuoteAndCommentReplacer {
+    private int quoteOpenIndex;
+    private int multiLineOpenCommentIndex;
+    private int multiLineOpenLine;
+    private boolean openQuote = false;
+    private boolean openMultiLineComment = false;
+    private boolean openJavadocComment = false;
+    private boolean afterSingleLineComment = false;  // Needed for removing comments after // - this broke somehow
+    private boolean previousCharacterIsEscape = false;
+    private List<int[]> charactersToRemove = new ArrayList<>();  //
+    private List<String> fileContents;  // Contents of file to find and replace text
+    //private List<String> modifiedFileContents;
+
+    QuoteAndCommentReplacer(List<String> contents) {
+        fileContents = contents;
+        //TODO: Initialize objects here (remove initialization from above)
+    }
+
+    public List getTransformedText() {
+
+        findTextToRemoveBounds();
+        replaceQuotedText();
+
+        return fileContents;
+    }
+
+    private void findTextToRemoveBounds() {
+        // Find text to replace on each line of the input file
+        for (int index = 0; index < fileContents.size(); index ++) {
+            removeQuotedAndCommentedText(index + 1, fileContents.get(index));
+        }
+    }
+
+    private void removeQuotedAndCommentedText(int lineNumber, String lineText) {
+
+        for (int index = 0; index < lineText.length(); index ++) {
+
+            /*
+             * Search for a quotation mark not preceded by an escape character.  If found add bounds of quoted text to
+             * an int array and add that array to the charactersToRemoveList and set openQuote to false;
+             */
+            if (openQuote) {
+                if (lineText.charAt(index) == '\"') {
+                    if (previousCharacterIsEscape) {
+                        previousCharacterIsEscape = false;
+                    } else {
+                        int[] tempArray = {lineNumber, quoteOpenIndex, lineNumber, index};
+                        charactersToRemove.add(tempArray);
+                        // TODO: Remove this line for when debug complete
+                        //quotedTextArray.add(lineText.substring(quoteOpenIndex + 1, index));
+                        openQuote = false;
+                    }
+
+                } else if (lineText.charAt(index) == '\\') {
+                    previousCharacterIsEscape = true;
+                }
+            /*
+             * Search for a closing javadoc symbol.
+             */
+            }  else if (openJavadocComment) {
+                // Check for first character of closing comment
+                if (lineText.charAt(index) == '*' && (index < lineText.length() - 1)) {
+                    // If first character is found, check it isn't the last character in the string, and that the next
+                    // character is the second character of the closing comment
+                    if (lineText.charAt(index + 1) == '/') {
+                        System.out.println("Found closing javadoc");
+                        int[] tempArray = {multiLineOpenLine, multiLineOpenCommentIndex, lineNumber, index};
+                        charactersToRemove.add(tempArray);
+                        openJavadocComment = false;
+                    }
+                }
+            }
+            else if (openMultiLineComment) {
+                // Check for first character of closing comment
+                if (lineText.charAt(index) == '*' && (index < lineText.length() - 1)) {
+                    // If first character is found, check it isn't the last character in the string, and that the next
+                    // character is the second character of the closing comment
+                    if (lineText.charAt(index + 1) == '/') {
+                        System.out.println("Found closing multi-line comment");
+                        int[] tempArray = {multiLineOpenLine, multiLineOpenCommentIndex, lineNumber, index};
+                        charactersToRemove.add(tempArray);
+                        openMultiLineComment = false;
+                    }
+                }
+            }
+            else {
+                // Set index and booleans if quote found
+                if (lineText.charAt(index) == '\"') {
+                    openQuote = true;
+                    quoteOpenIndex = index;
+                }
+
+                // Look for multi-line comments and javadocs, if there are at least two characters following the /
+                else if (lineText.charAt(index) == '/' ) {
+                    if (index < lineText.length() - 2) {
+
+                        // If followed by two * then it's a javadoc
+                        if (lineText.charAt(index + 1) == '*' && lineText.charAt(index + 2) == '*') {
+                            System.out.println("FOUND JAVADOC COMMENT ONE LINE: " + lineNumber);
+                            openJavadocComment = true;
+                            multiLineOpenCommentIndex = index + 2;
+                            multiLineOpenLine = lineNumber;
+                            index += 2;
+                        }
+
+                        // If followed by one * then it's a multi-line comment
+                        else if (lineText.charAt(index + 1) == '*') {
+                            System.out.println("FOUND MULTI-LINE COMMENT ONE LINE: " + index);
+                            openMultiLineComment = true;
+                            multiLineOpenCommentIndex = index + 1;
+                            multiLineOpenLine = lineNumber;
+                            index += 1;
+                        } else if (lineText.charAt(index + 1) == '/') {
+                            System.out.println("FOUND SINGLE ON LINE: " + lineNumber);
+                            System.out.println("Line Number: " + lineNumber + " start Index: " + (index + 2) + " Line Number: " + lineNumber + ". Line Length: " + (lineText.length() - 1));
+                            int[] tempArray = {lineNumber, index + 2, lineNumber, lineText.length() - 1};
+                            charactersToRemove.add(tempArray);
+                            return;
+                        }
+                    }
+
+                    // Check only for multi-line comment if there is only one character following the /
+                    else if (index < lineText.length() - 1) {
+                        if (lineText.charAt(index + 1) == '*') {
+                            System.out.println("FOUND MULTI-LINE COMMENT ON LINE: " + lineNumber);
+                            openMultiLineComment = true;
+                            multiLineOpenCommentIndex = index + 1;
+                            multiLineOpenLine = lineNumber;
+                            index += 1;
+                        } else if (lineText.charAt(index + 1) == '/') {
+                            System.out.println("FOUND SINGLE LINE COMMENT ON LINE: " + lineNumber);
+                            int[] tempArray = {lineNumber, index, lineNumber, lineText.length() - 1};
+                            charactersToRemove.add(tempArray);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void replaceQuotedText() {
+        for(int index = 0; index < charactersToRemove.size(); index ++) {
+
+            int[] tmpArray = charactersToRemove.get(index);
+
+            // If the text to replace spans only one line, use this simple code.  Else, remove text from each line.
+            if (tmpArray[0] == tmpArray[2]) {
+                if (tmpArray[1] >= tmpArray[3]) {
+                    String replacement = buildDots(tmpArray[1], tmpArray[3]);
+
+                    String lineText = fileContents.get(tmpArray[0] - 1);
+                    String newText = lineText.substring(0, tmpArray[1]) + replacement + lineText.substring(tmpArray[3] + 1);
+
+                    fileContents.set(tmpArray[0] - 1, newText);
+
+                    System.out.println(newText);
+                }
+
+            } else {
+
+                // Replace all text between the first line number first index and second line number second index
+                for (int currentLineNumber = tmpArray[0]; currentLineNumber <= tmpArray[2]; currentLineNumber ++) {
+
+                    // If this is the last line of text to replace, replace from index 0 to the closing index of the comment
+                    if (currentLineNumber == tmpArray[2]) {
+                        String replacement = buildDots(0, tmpArray[3]);
+                        String lineText = fileContents.get(currentLineNumber - 1);
+
+                        String newText = replacement + lineText.substring(tmpArray[3] );
+
+                        fileContents.set(tmpArray[2] - 1, newText);
+
+                        System.out.println("Line Number: " + currentLineNumber + ". New text: " + newText);
+                    }
+
+                    // If this is the first line of text to replace, replace from the opening index to the end of the string
+                    else if (currentLineNumber == tmpArray[0]) {
+                        String lineText = fileContents.get(currentLineNumber - 1);
+                        String replacement = buildDots(tmpArray[1], lineText.length() - 1);
+
+                        String newText = lineText.substring(0, tmpArray[1]) + replacement;
+
+                        fileContents.set(tmpArray[0] -1, newText);
+                        System.out.println("Line Number: " + currentLineNumber + ". New text: " + newText);
+                    }
+
+                    // This is a middle line that should entirely be replaced because it is entirely encompassed by
+                    // a multi-line comment
+                    else {
+                        String lineText = fileContents.get(currentLineNumber - 1 );
+                        String replacement = buildDots(0, lineText.length() - 1);
+
+                        fileContents.set(currentLineNumber - 1, replacement);
+                        System.out.println("Line Number: " + currentLineNumber + ". New text: " + replacement);
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    private String buildDots(int startIndex, int endIndex) {
+        String dots = "";
+        for (int i = 0; i < (endIndex - startIndex); i ++) {
+            dots += ".";
+        }
+
+        return dots;
+    }
+}
