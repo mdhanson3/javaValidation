5de318c3c310aa3ef06efadb2be5c8b715dd62b2
Mitchell Hanson <mitchhanson43@gmail.com>
2016-04-27 11:53:24 -0500

    changed file writer code to encode as utf-8

--- src/com/hanson/javaValidation/ErrorFileWriter.java
+++ src/com/hanson/javaValidation/ErrorFileWriter.java
@@ -9,7 +9,7 @@ import java.io.FileReader;
  * Created by student on 2/9/16.
  */
 public class ErrorFileWriter {
-    private final String HTML_STYLE = "<head><meta charset=\"utf-16\"/><style>.underline {text-decoration: underline; -moz-text-decoration-color: red; /* Code for Firefox */ text-decoration-color: red;} </style> </head>";
+    private final String HTML_STYLE = "<head><meta charset=\"utf-8\"/><style>.underline {text-decoration: underline; -moz-text-decoration-color: red; text-decoration-color: red; background-color: yellow;} </style> </head>";
 
     public void writeMarkupFile(String file) {
         String line = null;
@@ -48,19 +48,37 @@ public class ErrorFileWriter {
     }
 
     public void writeFileFromArray(List<String> output, String fileName) {
+        try {
+            BufferedWriter out = null;
+            try /*(BufferedWriter out = new BufferedWriter(new FileWriter("output/" + fileName)))*/ {
+                System.out.println("==================IN WRITER=====================");
 
-        try(BufferedWriter out = new BufferedWriter(new java.io.FileWriter("output/" + fileName))) {
-            out.write(HTML_STYLE);
-           for(String outputLine : output) {
-               out.write(outputLine + "<br />");
-               out.newLine();
-           }
-        } catch (IOException ioException) {
-            System.out.println("Error writing the markup file");
-            ioException.printStackTrace();
-        } catch (Exception exception) {
-            System.out.println("Error writing the cleansed file");
-            exception.printStackTrace();
+                File outputFile = new File("output/" + fileName);
+                if (!outputFile.exists()) {
+                    outputFile.createNewFile();
+                }
+
+                out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8"));
+
+                out.write(HTML_STYLE);
+                for (String outputLine : output) {
+                    System.out.println("writing");
+                    out.write(outputLine + "<br />");
+                    out.newLine();
+                }
+            } catch (IOException ioException) {
+                System.out.println("Error writing the markup file");
+                ioException.printStackTrace();
+            } catch (Exception exception) {
+                System.out.println("Error writing the cleansed file");
+                exception.printStackTrace();
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+            }
+        } catch (Exception e) {
+
         }
     }
 }
